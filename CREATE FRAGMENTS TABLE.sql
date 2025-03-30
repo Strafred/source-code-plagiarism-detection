9f917ec7b8aceb80b3c2b29c36bc4decd4b881dd
Strafred <bred7355608@gmail.com>
2025-03-30 23:08:56 +0300

    add prototype

---
+++ CREATE FRAGMENTS TABLE.sql
@@ -0,0 +1,5 @@
+CREATE table unique_fragments (
+                                  fragment_id serial primary key,
+                                  fragment text,
+                                  fragment_embedding vector(768)
+)
