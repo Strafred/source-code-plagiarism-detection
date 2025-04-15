36c09fb18d6b537180c3276267a3eb1baf9a22aa
Strafred <bred7355608@gmail.com>
2025-04-15 17:48:26 +0300

    добавлена балансировка классов свой-чужой

---
+++ plagiarism_dataset/case-03/non-plagiarized/24_1/L4.java
@@ -0,0 +1,12 @@
+public class L4 {
+    public static void print() {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+    public static void main(String[] args) {
+        print();
+    }
+}
