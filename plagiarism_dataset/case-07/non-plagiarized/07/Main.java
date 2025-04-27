8a72e638effd05a4a46e4f624f47f766e6512afd
Strafred <bred7355608@gmail.com>
2025-04-27 15:14:31 +0300

    add all counter examples for train and test

--- plagiarism_dataset/case-07/non-plagiarized/07/Main.java
+++ plagiarism_dataset/case-07/non-plagiarized/07/Main.java
@@ -1,17 +1,40 @@
+import java.text.DecimalFormat;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a[]=new int[10];
-        for (int i=0;i<10;i++)
+        double a[][] =new double[4][4];
+        for (int i=0;i<4;i++)
         {
-            System.out.print("Read a number: ");
-            int number = sc.nextInt();
-            a[i]=number;
-        }
-        for (int i=9;i>=0;i--)
+            for (int j=0;j<4;j++)
+            {
+                 a[i][j]=sc.nextDouble();
+            }
+          }
+        for (int i=0;i<4;i++)
         {
-            System.out.println(a[i]);
+            for (int j=0;j<4;j++)
+            {   if(a[i][j]%1==0){
+                    DecimalFormat decimalFormat = new DecimalFormat("#");
+                    String numberAsString = decimalFormat.format(a[i][j]);
+                    System.out.print(numberAsString);
+                }
+                else{
+                        System.out.print(a[i][j]);
+                }
+                System.out.print(" ");
+            }
+              System.out.println("");
         }
+        System.out.println("Sum of the elements in the major diagonal is "+sumMajorDiagonal(a));
     }
+public static double sumMajorDiagonal(double[][] array)
+{
+    double total = 0;
+    for (int index = 0; index < array.length; index++)
+    {
+            total += array[index][index];
+    }
+    return total;
+}
 }
