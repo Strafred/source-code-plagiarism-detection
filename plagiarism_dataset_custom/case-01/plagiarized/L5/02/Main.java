b3334e1d962fac4303957cc03af8b779407b601c
Strafred <bred7355608@gmail.com>
2025-04-27 16:00:39 +0300

    make getting data faster

---
+++ plagiarism_dataset_custom/case-01/plagiarized/L5/02/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) { 
+        cetak();
+    }
+    public static void cetak(){
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java"); 
+        }
+    }
+}
