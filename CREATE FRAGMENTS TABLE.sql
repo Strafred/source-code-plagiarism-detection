0c61d895efe1bf843799c1180c0ebcef1070b29e
Strafred <bred7355608@gmail.com>
2025-05-18 21:43:02 +0300

    add hnsw index

--- CREATE FRAGMENTS TABLE.sql
+++ CREATE FRAGMENTS TABLE.sql
@@ -2,4 +2,6 @@ CREATE table unique_fragments (
                                   fragment_id serial primary key,
                                   fragment text,
                                   fragment_embedding vector(1024)
-)
+);
+CREATE INDEX ON unique_fragments USING hnsw (fragment_embedding vector_cosine_ops);
+
